f7c605cdcda5ace3dec1315e21be17d28edd5c87
priyankacse503 <priyankacse503>
2022-12-10 08:17:56 +0530

    Filter Commit

---
+++ Filter.js
@@ -0,0 +1,87 @@
+
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+var filter=document.getElementById('filter');
+// Form submit event
+form.addEventListener('submit', addItem);
+// Delete event
+itemList.addEventListener('click', removeItem);
+//Filter event
+filter.addEventListener('keyup',filterItems);
+// Add item
+function addItem(e){
+  e.preventDefault();
+
+  // Get input value
+  var newItem = document.getElementById('item').value;
+
+  // Create new li element
+  var li = document.createElement('li');
+  // Add class
+  li.className = 'list-group-item';
+  // Add text node with input value
+  // Get Description input value
+  var newDesItem = document.getElementById('description').value;
+  li.appendChild(document.createTextNode(newItem+" "+newDesItem));
+
+  // Create del button element
+  var deleteBtn = document.createElement('button');
+  // Add id to delbtn
+  deleteBtn.id='deleteid';
+  // Add classes to del button
+  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+
+  // Append text node
+  deleteBtn.appendChild(document.createTextNode('X'));
+
+  // Append button to li
+  li.appendChild(deleteBtn);
+
+  // Append li to list
+  itemList.appendChild(li);
+
+  //Add Edit button
+  var editBtn = document.createElement('button');
+  // Add id to editbtn
+  editBtn.id='editbtn';
+  // Add classes to edit button
+  editBtn.className = 'btn btn-sm float-right editbtn';
+
+  // Append text node
+  editBtn.appendChild(document.createTextNode('Edit'));
+
+  // Append button to li
+  li.appendChild(editBtn);
+
+  // Append li to list
+  itemList.appendChild(li);
+}
+// Remove item
+function removeItem(e){
+  if(e.target.classList.contains('delete')){
+    if(confirm('Are You Sure?')){
+      var li = e.target.parentElement;
+      itemList.removeChild(li);
+    }
+  }
+}
+//Filter Items
+function filterItems(e)
+{
+//convert text to lowerCase
+var text=e.target.value.toLowerCase();
+//get list
+var items=itemList.getElementsByTagName('li');
+//convert to an array
+Array.from(items).forEach(function(item){
+    var itemName=item.firstChild.textContent;
+    if(itemName.toLowerCase().indexOf(text)!=-1){
+        item.style.display='block';
+    }
+    else{
+        item.style.display='none';
+    }
+});
+}
+
+
